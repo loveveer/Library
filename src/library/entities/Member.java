0a9786a5686def55f7f817babd88242d4b6dc2fd
Prashant Sharma <prashantsharmabhandari@gmail.com>
2016-09-29 23:18:50 +1000

    Declared and initialized constants and variables, implemented hasFinesPayable(), hasReachedFineLimit(), and getFineAmount() functions

--- src/library/entities/Member.java
+++ src/library/entities/Member.java
@@ -8,13 +8,18 @@ import java.util.List;
 
 public interface Member {
 	public static final int LOAN_LIMIT = 5;
+	//setting maximum fine constant to 5
+	public static final int FINE_MAX = 5;
 	
 	private int id;
 	private string firstName;
 	private string lastName;
 	private string contactPhone;
 	private string emailAddress;
-	private float totalFines;
+	//declaring total fines var and initialing it to 0;
+	private float totalFines = 0;
+	//declaring fines counter var and initialing it to 0;
+	private int finesCount = 0;
 	private ILoan loanList[LOAN_LIMIT];
 		
 	public boolean hasOverDueLoans(){
@@ -29,10 +34,41 @@ public interface Member {
 		retrun true;
 	}
 	
-	public boolean hasFinesPayable();
-	public boolean hasReachedFineLimit();
-	public float   getFineAmount();
-	public void    addFine(float fine);
+	/* 	
+	returns true if the fines currently owed by the Member exceeds zero 
+	returns false otherwise 
+	*/
+	public boolean hasFinesPayable(){
+		if finesCount > 0
+			return true;
+		return false;
+	}
+	
+	/*
+	returns true if	the fines owing by the Member is equals or is greater than FINE_MAX 
+	returns false otherwise 
+	*/
+	public boolean hasReachedFineLimit(){
+		if finesCount == FINE_MAX
+			return true;
+		return false;
+	}
+	
+	/*
+	returns the amount of fines owing by the Member 
+	*/
+	public float   getFineAmount(){
+		return totalFines;
+	}
+	
+	/*
+	Increments the fines owing by amount 
+	*/
+	public void    addFine(float fine){
+		totalFines = totalFines + fine;
+		finesCount = finesCount +1;
+	}
+	
 	public void    payFine(float payment);
 	public void    addLoan(ILoan loan);
 	public List<ILoan> getLoans();
