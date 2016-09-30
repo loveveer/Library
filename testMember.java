be0c0cae36a292b8af29fdf9c93cb812fe38912e
Loveveer Singh <luvveer33@gmail.com>
2016-09-30 18:30:54 +1000

    Added few more unit tests for our Member entity class. Contributed unit tests for addFines(), payFines(), addLoan(), and removeLoan()

--- testMember.java
+++ testMember.java
@@ -19,14 +19,55 @@ public class TestMember {
         member[0] = member.addMember("Abhishek", "Gaba", "0451222334", "email@gmail.com");
         member[1] = member.addMember("Lakhveer", "Singh", "0451234454", "email@gmail.com");
     }                   
-     
+
+	/*
+	Unit test for  hasFinesPayable() method of entity class Member
+	*/	
 	@Test
     public void testHasFinesPayable() {
         member[0].finesCount = 2;
 		member[0].hasFinesPayable();
         assertTrue();
     }
-	 
+	
+	/*
+	Unit test for testAddFines() method of entity class Member
+	*/	 
+	@Test
+    public void testAddFines() {
+        member[1].addFine(1000);
+		member[1].addFine(1500);
+        member[1].addFine(500);        
+        assertEquals(3000, member[1].totalFines);
+    } 
+	
+	/*
+	Unit test for payFines() method of entity class Member
+	*/
+	@Test
+    public void testPayFines() {
+        member[1].payFines(1000);
+		assertEquals(2000, member[1].totalFines);
+    }
+	
+	/*
+	Unit test for addLoan() method of entity class Member
+	*/
+	@Test
+    public void testAddLoan() {
+        member[1].payFines(1000);
+		assertEquals(2000, member[1].totalFines);
+    }
+	
+	/*
+	Unit test for removeLoan() method of entity class Member
+	*/
+	@Test
+    public void testRemoveLoan() {
+        member[1].payFines(1000);
+		assertEquals(2000, member[1].totalFines);
+    }
+	
     @Test
     public void testHasOverdueLoans() {
         //arrange
